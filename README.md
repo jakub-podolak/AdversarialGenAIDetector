1c357a8680ba0bd22200bc50d41c25a748f58696
Jakub Podolak <scur1745@gcn59.local.snellius.surf.nl>
2024-09-21 00:52:52 +0200

    add env and notebooks

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # AdversarialGenAIDetector
 Project for DL4NLP at UVA (2024/2025)
+
+## 1. Running on Lisa / Snellius
+
+Install environment using
+```bash
+sbatch install_environment.job
+```
+
+Run interactive session
+```bash
+srun --partition=gpu --gpus=1 --ntasks=1 --cpus-per-task=18 --time=04:00:00 --pty bash -i
+```
+
+And later
+```bash
+module purge
+module load 2022
+module load Anaconda3/2022.05
+
+source activate dl4nlp_gpu
+```
---
+++ data/datasets.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from dataclasses import dataclass
+
+REQUIRED_COLUMNS: list[str] = ["text", "label"]
+
+@dataclass
+class Dataset:
+    # Dataframe with columns ['text', 'label'] where
+    # label = 1 means that text is AI generated
+    dataframe: pd.DataFrame
+
+    def __init__(self, raw_data: pd.DataFrame):
+        for column in REQUIRED_COLUMNS:
+            assert column in raw_data.columns
+
+        self.dataframe = raw_data
+
+
+# I recommend to create functions like this one for new datasets
+def create_sample_dataset() -> Dataset:
+    df = pd.DataFrame([
+        {"text": "Man i just love cooking spathetti with my friends xd",
+         "label": 0},
+        {"text": "Cooking spaghetti with friends is more than just making a"
+                  "meal—it's a shared experience of joy and connection.",
+         "label": 1}
+    ])
+    return Dataset(df)
+
+
+# Register all the dataset here, so we can just use
+# `dataset = DATASETS["sample_dataset"]()` in code
+DATASETS: dict[str, callable] = {
+    "sample_dataset": create_sample_dataset
+}
